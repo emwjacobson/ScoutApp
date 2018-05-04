784df1ec6bace04fe2583add7c0137c980d9fe80
Emerson Jacobson <emwjacobson@gmail.com>
2018-05-03 21:11:49 -0700

    Added randomColor and basic graph for plotting overall team proformance.

--- src/app/pages/analysis/analysis.component.ts
+++ src/app/pages/analysis/analysis.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
+const randomColor = require('randomcolor');
 
 @Component({
   selector: 'app-analysis',
@@ -7,6 +8,14 @@ import { BackendService } from '../../services/backend.service';
   styleUrls: ['./analysis.component.less']
 })
 export class AnalysisComponent implements OnInit {
+  public lineChartData: Array<any> = [
+    { data: [ {x: -10, y: 0}, { x: 0, y: 10 }, { x: 10, y: 5 } ], label: 'Idk', pointRadius: 5, pointBackgroundColor: randomColor() },
+    { data: [ {x: -10, y: 0}, {x: 0, y: 4}, { x: 2, y: 18 }, { x: 3, y: 57 } ], label: 'Idk', pointRadius: 5, pointBackgroundColor: randomColor()  },
+  ];
+  public lineChartOptions: any = {
+    responsive: true
+  };
+
   public radarChartLabels: string[] = [];
   public radarChartData: any = [{ data: [], label: '' }];
   public radarOptions: any = {
@@ -16,7 +25,7 @@ export class AnalysisComponent implements OnInit {
       }
     }
   };
-  public showChart = false;
+  public showRadar = false;
 
   constructor(private backend: BackendService) { }
 
@@ -27,14 +36,18 @@ export class AnalysisComponent implements OnInit {
     return this.backend.getCurRegional().teams;
   }
 
-  public updateChart(e) {
+  public updateLine(e) {
+
+  }
+
+  public updateRadar(e) {
     const team_number = e.target.value;
     const data = this.getMatchStats(team_number);
     if (!data) {
-      this.showChart = false;
+      this.showRadar = false;
       return;
     } else {
-      this.showChart = true;
+      this.showRadar = true;
     }
     this.radarChartData = this.getValues(data);
 
